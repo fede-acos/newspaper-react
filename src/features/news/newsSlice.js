974777dc96a9f2b49fe58b068d94b047ef5e140b
Federico Acosta <fede.acosta2396@gmail.com>
2022-12-21 21:32:20 -0300

    1

---
+++ src/features/news/newsSlice.js
@@ -0,0 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initalState = {
+  count: 0,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,7 +18,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={true} />
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </QueryClientProvider>
   </React.StrictMode>
 );
