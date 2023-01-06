53c6b0a31ec5b825a4c4bdfd9626b3f2a47de702
Federico Acosta <fede.acosta2396@gmail.com>
2023-01-06 16:15:42 -0300

    need to fix bug when changing news section

--- src/componets/RelatedNews.jsx
+++ src/componets/RelatedNews.jsx
@@ -39,7 +39,27 @@ function RelatedNews() {
     <div className=" xl:pr-10 xl:pl-10 2xl:pl-24 2xl:pr-24">
       {relatedNews &&
         relatedNews?.map((news) => {
-          return <IndividualNews key={news.url} news={news} />;
+          return (
+            <article key={news.url} className="lg:mt-8 mt-4 ">
+              <div className=" card flex-row-reverse  bg-base-100 shadow-xl p-2">
+                <a className="hover:underline " href={news.url}>
+                  <h2 className="card-title md:items-start">{news.title}</h2>
+                </a>
+                <figure className="relative items-start">
+                  <img
+                    className="rounded max-w-lg"
+                    src={news.multimedia[0].url}
+                    alt={news.multimedia[0].caption}
+                  />
+                  <div className="absolute bottom-0 left-0 bg-blue-700 p-1.5 font-bold rounded">
+                    <h1 className="text-white text-sm ">
+                      {news.section.toUpperCase()}
+                    </h1>
+                  </div>
+                </figure>
+              </div>
+            </article>
+          );
         })}
       <div className="w-full flex place-content-center mt-8 pb-16">
         <button
