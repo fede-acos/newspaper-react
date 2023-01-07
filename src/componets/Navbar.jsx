983da74ac37ec6f621d16077480eb7fac63f7d38
Federico Acosta <fede.acosta2396@gmail.com>
2023-01-07 12:58:12 -0300

    fixed resposive bugs

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { themeChange } from "theme-change";
 import NavBarItems from "./NavBarItems";
-
+import BtnDarkModeToggle from "./BtnDarkModeToggle";
 const Navbar = () => {
   const [toggleNavbar, setToggleNavbar] = useState(false);
   const [theme, setTheme] = useState("light");
@@ -25,10 +25,10 @@ const Navbar = () => {
   return (
     <>
       <nav className="navbar justify-between bg-base-100 pt-6">
-        <div className="navbar-start">
-          <div className="dropdown">
+        <div className="navbar-start w-[100%] place-content-center md:justify-start md:w-[50%]">
+          <div className="dropdown ">
             <button
-              className="btn btn-ghost 2xl:hidden"
+              className="btn btn-ghost 2xl:hidden xl:pl-4 xl:pr-4 xl:h-12 pl-2 pr-2 h-8"
               onClick={() => handleClick()}
             >
               <svg
@@ -47,16 +47,21 @@ const Navbar = () => {
               </svg>
             </button>
             {toggleNavbar ? (
-              <ul className="menu menu-compact mt-3 p-2 shadow-xl bg-base-100 rounded-box w-52 absolute z-10">
+              <ul className="menu text-  mt-3 p-2 shadow-xl bg-base-100 rounded-box w-52 absolute z-10">
                 <NavBarItems />
               </ul>
             ) : (
               <div />
             )}
           </div>
-          <a className="btn btn-ghost normal-case text-3xl 2xl:text-5xl sm:text-4xl ml-[2%]">
+          <a className="btn btn-ghost normal-case text-3xl 2xl:text-5xl sm:text-4xl ml-[2%] xl:pl-4 xl:pr-4  pl-3 pr-3 ">
             The News Times
           </a>
+          <BtnDarkModeToggle
+            style={"pr-2 hover:opacity-90 md:hidden relative top-1 "}
+            theme={theme}
+            handleThemeChange={handleThemeChange}
+          />
         </div>
         <div className="navbar-center hidden 2xl:flex">
           <ul className="menu menu-horizontal px-1">
@@ -66,28 +71,12 @@ const Navbar = () => {
         <div className="navbar-end hidden md:flex pr-4 ">
           <a className="btn">Subscribe</a>
         </div>
-        <div className="pr-2 hover:opacity-90">
-          <button
-            onClick={() => handleThemeChange()}
-            data-set-theme={theme}
-            data-act-class="ACTIVECLASS"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-              />
-            </svg>
-          </button>
-        </div>
+
+        <BtnDarkModeToggle
+          style={"pr-2 hover:opacity-90 hidden md:block"}
+          theme={theme}
+          handleThemeChange={handleThemeChange}
+        />
       </nav>
     </>
   );
