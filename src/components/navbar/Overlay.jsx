af7e2e9a6e5ceb54eb8fa3227006c7006bdb14f3
Federico Acosta <fede.acosta2396@gmail.com>
2023-03-08 19:59:03 -0300

    mobile navbar fixes

--- src/components/navbar/Overlay.jsx
+++ src/components/navbar/Overlay.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 import items from "./itemsNavbar";
 
 function Overlay({
@@ -8,32 +8,54 @@ function Overlay({
   isDropDownOpen,
   handleSectionChange,
 }) {
+  const { section } = useSelector((state) => state.section);
+
   return (
     <div
-      className={`fixed top-0 left-0 z-10 h-full w-[50%]  -translate-x-full bg-slate-900 md:w-[40%] lg:w-[30%] xl:w-[20%] ${
+      className={`fixed top-0 left-0 z-10 h-full w-[50%]  -translate-x-full bg-base-100 md:w-[40%] lg:w-[30%] xl:w-[20%] ${
         isDropDownOpen ? "translate-x-0" : ""
       } `}
     >
       <button
-        className="absolute top-4 right-9 text-4xl text-white"
+        className="absolute top-5 right-4  opacity-90"
         onClick={() => handleClick()}
         ref={dropdownRef}
       >
-        X
+        <svg
+          viewBox="0 0 38 38"
+          width="20px"
+          height="20px"
+          aria-hidden="true'"
+          className="fill-current"
+        >
+          <g stroke="none" stroke-width="1" fill-rule="evenodd">
+            <polygon
+              fill-rule="nonzero"
+              points="33.4 0 19 14.4 4.601 0 0 4.6 14.4 19 0 33.4 4.6 38 19 23.6 33.4 38 38 33.399 23.6 18.999 38 4.6"
+            ></polygon>
+          </g>
+        </svg>
       </button>
-      <ul className=" absolute top-20 flex w-full flex-col items-center gap-2 ">
+      <ul className=" absolute top-20 flex w-full flex-col items-center p-2 ">
+        <span className="mb-4 h-[1px] w-[90%] bg-base-content opacity-20"></span>
         {items.map((item) => (
-          <li
-            key={item.name}
-            className="w-full text-center text-xl text-white sm:text-2xl"
-          >
-            <a
-              className="cursor-pointer"
-              onClick={() => handleSectionChange(item.value)}
+          <>
+            <li
+              key={item.name}
+              className={`text w-full rounded-md p-2 pl-4 text-left text-xl text-base-content sm:text-2xl ${
+                section === item.value
+                  ? "bg-slate-700 text-gray-50 dark:bg-slate-400 dark:text-zinc-800"
+                  : "hover:bg-base-200  focus:bg-base-200   active:bg-base-200"
+              }`}
             >
-              {item.name}
-            </a>
-          </li>
+              <a
+                className="cursor-pointer"
+                onClick={() => handleSectionChange(item.value)}
+              >
+                {item.name}
+              </a>
+            </li>
+          </>
         ))}
       </ul>
     </div>
